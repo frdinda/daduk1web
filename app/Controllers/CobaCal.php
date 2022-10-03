3bba4f998b490c210f4ce70407f08917a572d157
unknown <frdinda.47@gmail.com>
2022-10-03 15:41:20 +0700

    baru awal, semoga semangat

---
+++ app/Controllers/CobaCal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class CobaCal extends BaseController
+{
+    public function index()
+    {
+        return view('coba_cal');
+    }
+}
---
+++ app/Views/coba_cal.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Calendar</title>
+    <!-- css -->
+    <link rel="stylesheet" href="assets_1/fullcalendar.css">
+    <link rel="stylesheet" href="assets_1/bootstrap.css">
+    <!-- js -->
+    <script src="assets_1/jquery.min.js"></script>
+    <script src="assets_1/jquery-ui.min.js"></script>
+    <script src="assets_1/moment.min.js"></script>
+    <script src="assets_1/fullcalendar.min.js"></script>
+</head>
+
+<body>
+    <br>
+    <a href="#" class="text-center">
+        <h2>Membuat Kalendar Kegiatan dengan PHP, MySQL, dan Library Fullcalendar</h2>
+    </a>
+    <br>
+    <div class="container">
+        <div id="calendar">
+
+        </div>
+    </div>
+
+    <script>
+        // Persiapan JQuery
+        $(document).ready(function() {
+
+        })
+    </script>
+
+</body>
+
+</html>
